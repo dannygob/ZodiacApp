f6b75f473219caace7770c2d5e81b2fd3ba2bc29
tardes <dannygomezb@gmail.com>
2025-03-20 18:49:21 +0100

    Prepared all dar we need that we need for the proyect.

---
+++ app/src/main/java/com/example/zodiacapp/data/Horoscope.kt
@@ -0,0 +1,11 @@
+package com.example.zodiacapp.data
+
+class Horoscope (
+    //declaracion del signo con su identificador
+    val id: String,
+    val name: Int,
+    val dates: Int,
+    val icon: Int,
+){
+
+}
